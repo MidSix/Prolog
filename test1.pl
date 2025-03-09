4065817e399c8533a4f7a99f5170937ee0cb46c1
Sebastián <sebamorexx@gmail.com>
2025-03-09 12:24:02 +0100

    Doing prolog stuff

---
+++ test1.pl
@@ -0,0 +1,33 @@
+/* 
+Comentarios en Prolog : hay de dos tipos, comentarios de linea y de bloque. Los primeros empiezan con '%'
+los segundos empiezan y terminan como este comentario 
+*/
+
+/*
+Clauses:
+Clauses are like a broad term for the basics units of Prolog, everything is built from clauses
+then, what are the basic units? -> facts(also known as 'predicates'), rules and queries(?- ), a fact is a clause, a rule is a clause and a query is a clause. 
+*/
+
+/*
+Prolog data-base(also known as base de conocimiento):
+in practical terms, a prolog data-base is what we are writing here in this module, this is where queries are redirect to get an answer to
+our questions, this is why, if we don't import a data-base to prolog we get an error like the next:
+
+?- father(felipe, leonor).
+ERROR: Unknown procedure: father/2 (DWIM could not correct goal)
+
+father/2 means the fact with the relation name father with two arguments. Prolog could'nt find it, that's because 
+we didn't upload our database to prolog(even if we upload our data-base, if the data-base don't have that fact, we'll get the same error).
+
+with the symbol '?-' we are doing queries, asking, not defining facts, rules or whatever, so, we must be ensure that we have uploaded our data-base
+before doing a query.
+
+our program must be written in the data-base.
+*/
+
+%A prolog data-base
+father(felipe,leonor).
+father(felipe,juan_carlos).
+father(felipe,antonio).
+father(felipe,julieta).
